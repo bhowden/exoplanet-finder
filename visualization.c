c4c8bfcab65accc289b3580753f71248c0b1f85a
Ben Howden <bhowden83@gmail.com>
2023-08-15 14:36:32 -0400

    3d model

--- visualization.c
+++ visualization.c
@@ -1,3 +1,7 @@
+/*
+ * Generate a 3d model for computer screens based on exoplanet coordinates for visualization
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -10,13 +14,16 @@ struct CartesianCoordinates {
     double z;
 };
 
+// Converts galactic coordinates to Cartesian coordinates
 struct CartesianCoordinates convertToCartesian(struct Exoplanet exoplanet) {
     struct CartesianCoordinates cartesianCoord;
 
+    // Convert galactic coordinates to spherical coordinates
     double r = exoplanet.distance; // Assuming 'distance' is the radius in light years
     double theta = exoplanet.galacticLongitude * (M_PI / 180.0); // Convert degrees to radians
     double phi = exoplanet.galacticLatitude * (M_PI / 180.0); // Convert degrees to radians
 
+    // Convert spherical coordinates to Cartesian coordinates
     cartesianCoord.x = r * cos(theta) * cos(phi);
     cartesianCoord.y = r * sin(theta) * cos(phi);
     cartesianCoord.z = r * sin(phi);
@@ -24,29 +31,35 @@ struct CartesianCoordinates convertToCartesian(struct Exoplanet exoplanet) {
     return cartesianCoord;
 }
 
+// Scales Cartesian coordinates using a scaling factor
 void scaleCoordinates(struct CartesianCoordinates *coordinates, double scalingFactor) {
     coordinates->x *= scalingFactor;
     coordinates->y *= scalingFactor;
     coordinates->z *= scalingFactor;
 }
 
+// Calculates scaling factor for visualization
 double calculateScalingFactor(double maxDistance, double minDistance, double screenWidth, double screenHeight) {
     double distanceRange = maxDistance - minDistance;
     double visualizationWidth = screenWidth;
     double visualizationHeight = screenHeight;
 
+    // Calculate scaling factors for x, y, and z axes
     double scaleX = visualizationWidth / distanceRange;
     double scaleY = visualizationHeight / distanceRange;
     double scaleZ = 1.0; // You may adjust this based on your desired depth
 
+    // Use the smallest scaling factor to fit within the screen
     return fmin(scaleX, fmin(scaleY, scaleZ));
 }
 
-// don't forget to free me
+// Generates OBJ data for exoplanets and returns a pointer to the data
+// Memory management responsibility is with the caller
 char *generateObjData(struct Exoplanet *exoplanets, int numExoplanets, double screenWidth, double screenHeight, size_t *objSize) {
     double maxDistance = 0.0;
     double minDistance = DBL_MAX;
 
+    // Find maximum and minimum distances among exoplanets
     for (int i = 0; i < numExoplanets; i++) {
         if (exoplanets[i].distance > maxDistance) {
             maxDistance = exoplanets[i].distance;
@@ -56,8 +69,10 @@ char *generateObjData(struct Exoplanet *exoplanets, int numExoplanets, double sc
         }
     }
 
+    // Calculate scaling factor for visualization
     double scalingFactor = calculateScalingFactor(maxDistance, minDistance, screenWidth, screenHeight);
 
+    // Allocate memory for OBJ data
     size_t bufferSize = numExoplanets * 128; // Adjust buffer size as needed
     char *objData = (char *)malloc(bufferSize);
     if (!objData) {
@@ -65,11 +80,13 @@ char *generateObjData(struct Exoplanet *exoplanets, int numExoplanets, double sc
         return NULL;
     }
 
+    // Generate vertex data and format it as OBJ lines
     size_t offset = 0;
     for (int i = 0; i < numExoplanets; i++) {
         struct CartesianCoordinates cartesianCoord = convertToCartesian(exoplanets[i]);
         scaleCoordinates(&cartesianCoord, scalingFactor);
 
+        // Format the vertex line for OBJ format
         offset += snprintf(objData + offset, bufferSize - offset, "v %.6f %.6f %.6f\n", cartesianCoord.x, cartesianCoord.y, cartesianCoord.z);
     }
 
